ec125b11ffe3dac825a3247ef7b8bdec6a088ec3
Martin Wenisch <martin.wenisch@cesko.digital>
2021-07-09 10:41:53 +0200

    Terraform - AWS network, database, Github action for deployment

---
+++ infrastructure/aws/variables.tf
@@ -0,0 +1,25 @@
+variable "aws-region" {
+  type    = string
+  default = "eu-north-1"
+}
+
+variable "codename" {
+  type    = string
+  default = "loono"
+}
+
+# Internal domain, its not visible publicly, but it should be domain we own
+# to comply with best practices => it ensures the internal domain names are globally unique.
+variable "codename-domain" {
+  type    = string
+  default = "loono.ceskodigital.net"
+}
+
+variable "database-username" {
+  type    = string
+  default = "loono"
+}
+
+variable "database-password" {
+  type = string
+}
