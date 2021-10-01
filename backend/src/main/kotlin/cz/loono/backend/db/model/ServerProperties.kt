471f09e328229f626a4b5e8e75b28e4cafed7c63
Radek Poskocil <r.poskocil@quadient.com>
2021-10-01 13:35:57 +0200

    Added password encoder

--- backend/src/main/kotlin/cz/loono/backend/db/model/ServerProperties.kt
+++ backend/src/main/kotlin/cz/loono/backend/db/model/ServerProperties.kt
@@ -19,7 +19,7 @@ data class ServerProperties(
     val superUserName: String = "loonoAdmin",
 
     @Column(nullable = false, columnDefinition = "TEXT")
-    val superUserPassword: String = "{noop}pass",
+    val superUserPassword: String = "\$2a\$10\$mqCYPoDvwtA.7H6qrhvFNeX5nlaM1n7Pt//A5gS3V9UOqiuhquu0i",
 
     @Column(nullable = false, columnDefinition = "TEXT")
     val updateInterval: String = "0 0 1 * *" // once per month
--- backend/src/main/kotlin/cz/loono/backend/security/SecurityConfig.kt
+++ backend/src/main/kotlin/cz/loono/backend/security/SecurityConfig.kt
@@ -5,11 +5,14 @@ import cz.loono.backend.security.basic.SuperUserDetailsService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableWebSecurity
@@ -22,9 +25,21 @@ class SecurityConfig @Autowired constructor(
         return SuperUserDetailsService()
     }
 
+    @Bean
+    fun authProvider(): DaoAuthenticationProvider {
+        val authProvider = DaoAuthenticationProvider()
+        authProvider.setUserDetailsService(userDetailsService())
+        authProvider.setPasswordEncoder(encoder())
+        return authProvider
+    }
+
     @Autowired
     fun configureGlobal(authentication: AuthenticationManagerBuilder) {
         authentication.userDetailsService(userDetailsService())
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.authenticationProvider(authProvider())
     }
 
     override fun configure(http: HttpSecurity) {
@@ -35,5 +50,10 @@ class SecurityConfig @Autowired constructor(
             .authenticationEntryPoint(authenticationEntryPoint)
             .and()
             .csrf()
+    }
+
+    @Bean
+    fun encoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
     }
 }
