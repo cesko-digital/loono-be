ac31c726e34bfb91855ab3e05360255b86564830
dzaru <danielzaru@gmail.com>
2022-02-13 13:49:53 +0100

    LOON-463: Remove redundant code

--- backend/src/main/kotlin/cz/loono/backend/db/repository/BadgeRepository.kt
+++
@@ -1,9 +0,0 @@
-package cz.loono.backend.db.repository
-
-import cz.loono.backend.db.model.Badge
-import cz.loono.backend.db.model.BadgeCompositeKey
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface BadgeRepository : CrudRepository<Badge, BadgeCompositeKey>
