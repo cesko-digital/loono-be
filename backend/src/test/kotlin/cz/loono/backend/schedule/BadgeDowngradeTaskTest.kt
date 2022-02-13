4d6759969b601f41a72917e966ee0672e03bf1ca
dzaru <danielzaru@gmail.com>
2022-02-13 14:34:38 +0100

    LOON-463: Add test case for removing badge

--- backend/src/test/kotlin/cz/loono/backend/schedule/BadgeDowngradeTaskTest.kt
+++ backend/src/test/kotlin/cz/loono/backend/schedule/BadgeDowngradeTaskTest.kt
@@ -111,14 +111,35 @@ class BadgeDowngradeTaskTest(
 
     @Test
     fun `Should remove badge when level downgraded to zero`() {
-        val accounts = listOf("uuid1", "uuid2", "uuid3").map(::createAccount)
+        val account = createAccount("uuid1")
+        val badges = setOf(
+            Badge(
+                BadgeTypeDto.GLASSES.toString(),
+                1,
+                1,
+                account,
+                LocalDateTime.parse("2020-02-13T17:14:06.419")
+            )
+        )
+        accountRepository.save(account)
+        val withBadgesAndRecords = accountRepository.findByUid("uuid1")!!.copy(
+            badges = badges,
+            examinationRecords = listOf(
+                ExaminationRecord(
+                    type = ExaminationTypeEnumDto.OPHTHALMOLOGIST,
+                    plannedDate = LocalDateTime.parse("2020-02-11T17:14:06.419"),
+                    account = account,
+                    status = ExaminationStatusDto.CONFIRMED
+                )
+            )
+        )
+        accountRepository.save(withBadgesAndRecords)
 
-        accountRepository.saveAll(accounts)
         badgeDowngradeTask.run()
 
         val actual = accountRepository.findByUid("uuid1")!!.badges
 
-        assertThat(actual).isEqualTo(emptySet<Badge>())
+        assertThat(actual).isEmpty()
     }
 
     @TestConfiguration
