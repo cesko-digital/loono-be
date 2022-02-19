6712e764b60ee7d5ad1e8bc4f92a69d81f181a38
Radek Poskočil <r.poskocil@quadient.com>
2022-02-20 00:55:35 +0100

    Account refactoring

--- backend/src/main/kotlin/cz/loono/backend/db/repository/ExaminationRecordRepository.kt
+++ backend/src/main/kotlin/cz/loono/backend/db/repository/ExaminationRecordRepository.kt
@@ -3,7 +3,6 @@ package cz.loono.backend.db.repository
 import cz.loono.backend.db.model.Account
 import cz.loono.backend.db.model.ExaminationRecord
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
