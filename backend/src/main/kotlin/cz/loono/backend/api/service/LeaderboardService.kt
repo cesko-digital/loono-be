d4453186c76cdc354d72237d21b9f08b0a0f2705
Daniil Zaru <danielzaru@gmail.com>
2022-02-26 23:25:28 +0100

    fix-complexity: Fix complexity of leaderboard calculation

--- backend/src/main/kotlin/cz/loono/backend/api/service/LeaderboardService.kt
+++ backend/src/main/kotlin/cz/loono/backend/api/service/LeaderboardService.kt
@@ -12,44 +12,16 @@ class LeaderboardService(
     private val accountRepository: AccountRepository
 ) {
 
-    private val batchSize = 500
-
     fun getLeaderboard(uid: String): LeaderboardDto {
-
         val top3 = findTop3Accounts(uid)
-
-        val iterations = accountRepository.count() / batchSize
-        var userOrder = 0L
-        val peers = mutableListOf<LeaderboardUserDto>()
-        iter@ for (i in 0..iterations) {
-            val accountsBatch = accountRepository.findAllByOrderByPointsDesc(PageRequest.of(i.toInt(), batchSize))
-            for (j in accountsBatch.indices) {
-                if (accountsBatch[j].uid == uid) {
-                    userOrder = i * batchSize + j + 1
-                    if (j > 0) {
-                        peers.add(prepareLeaderboardUser(uid, accountsBatch[j - 1]))
-                    } else if (i > 0) {
-                        val previousPage =
-                            accountRepository.findAllByOrderByPointsDesc(PageRequest.of((i - 1).toInt(), batchSize))
-                        peers.add(prepareLeaderboardUser(uid, previousPage[batchSize - 1]))
-                    }
-                    peers.add(prepareLeaderboardUser(uid, accountsBatch[j]))
-                    if (accountsBatch.size > 2 && j < batchSize - 1) {
-                        peers.add(prepareLeaderboardUser(uid, accountsBatch[j + 1]))
-                    } else if (accountsBatch.size == batchSize) {
-                        val nextPage =
-                            accountRepository.findAllByOrderByPointsDesc(PageRequest.of((i + 1).toInt(), batchSize))
-                        peers.add(prepareLeaderboardUser(uid, nextPage[0]))
-                    }
-                    break@iter
-                }
-            }
-        }
+        val currentAcc = accountRepository.findByUid(uid)
+        val myOrder = currentAcc?.points?.let { accountRepository.findMyPosition(currentAcc.points) + 1 } ?: 0
+        val peers = accountRepository.findPeers(currentAcc?.points ?: 0).map { prepareLeaderboardUser(uid, it) }
 
         return LeaderboardDto(
             top = top3,
             peers = peers,
-            myOrder = userOrder.toInt()
+            myOrder = myOrder
         )
     }
 
