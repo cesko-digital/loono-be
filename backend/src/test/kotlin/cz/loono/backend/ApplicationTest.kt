38550ee2ebb9ecd815463c16e43493344454233b
Daniil Zaru <danielzaru@gmail.com>
2022-03-13 00:23:34 +0100

    LOON-510: Remove redundant whitespace

--- backend/src/test/kotlin/cz/loono/backend/ApplicationTest.kt
+++ backend/src/test/kotlin/cz/loono/backend/ApplicationTest.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest(properties = ["spring.profiles.active=test"])
-
 class ApplicationTest {
 
     @Test
