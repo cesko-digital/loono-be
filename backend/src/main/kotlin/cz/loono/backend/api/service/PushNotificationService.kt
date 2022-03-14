74806a86710af99f7fbda40d3eccacd0e0396119
Radek Poskočil <r.poskocil@quadient.com>
2022-03-14 11:41:56 +0100

    LOON-394: Notifications

--- backend/src/main/kotlin/cz/loono/backend/api/service/PushNotificationService.kt
+++ backend/src/main/kotlin/cz/loono/backend/api/service/PushNotificationService.kt
@@ -96,15 +96,13 @@ class PushNotificationService {
         return Gson().fromJson(call.execute().body!!.string(), NotificationResponse::class.java).id
     }
 
-    private fun Request.Builder.addAuthenticationHeader(): Request.Builder = this.addHeader("Authorization", "Basic $ONESIGNAL_API_KEY")
+    private fun Request.Builder.addAuthenticationHeader(): Request.Builder =
+        this.addHeader("Authorization", "Basic $ONESIGNAL_API_KEY")
 
-    private fun Request.Builder.addContentTypeHeader(): Request.Builder {
-        return this.addHeader("Content-Type", "application/json; charset=utf-8")
-    }
+    private fun Request.Builder.addContentTypeHeader(): Request.Builder =
+        this.addHeader("Content-Type", "application/json; charset=utf-8")
 
-    private fun composeUrl(endpoint: String): String {
-        return "$ONESIGNAL_API_URL/$API_VERSION/$endpoint"
-    }
+    private fun composeUrl(endpoint: String): String = "$ONESIGNAL_API_URL/$API_VERSION/$endpoint"
 
     companion object {
         val ONESIGNAL_API_KEY: String = System.getenv("ONESIGNAL_API_KEY")
--- backend/src/main/kotlin/cz/loono/backend/notification/NotificationResponse.kt
+++ backend/src/main/kotlin/cz/loono/backend/notification/NotificationResponse.kt
@@ -3,5 +3,5 @@ package cz.loono.backend.notification
 data class NotificationResponse(
     val id: String,
     val recipients: Int,
-    val external_id: String?
+    val externalId: String?
 )
