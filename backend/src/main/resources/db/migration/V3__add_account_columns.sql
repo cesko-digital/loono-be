22cd9921ce261224d90a414df332e88fc2684982
Daniil Zaru <danielzaru@gmail.com>
2022-04-08 15:50:13 +0200

    rposkocil/new-columns: Allow aud table to have nullable fields

--- backend/src/main/resources/db/migration/V3__add_account_columns.sql
+++ backend/src/main/resources/db/migration/V3__add_account_columns.sql
@@ -2,10 +2,10 @@ ALTER TABLE account
     ADD COLUMN notify boolean NOT NULL DEFAULT true;
 
 ALTER TABLE account_aud
-    ADD COLUMN notify boolean NOT NULL DEFAULT true;
+    ADD COLUMN notify boolean;
 
 ALTER TABLE account
     ADD COLUMN active boolean NOT NULL DEFAULT true;
 
 ALTER TABLE account_aud
-    ADD COLUMN active boolean NOT NULL DEFAULT true;
+    ADD COLUMN active boolean;
