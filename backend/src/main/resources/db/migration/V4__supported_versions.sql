6928449fc74f7548d0631db9ef9bdc5f557b48ed
Radek Poskočil <r.poskocil@quadient.com>
2022-04-20 15:49:41 +0200

    LOON-541: App version check

--- backend/src/main/resources/db/migration/V4__supported_versions.sql
+++ backend/src/main/resources/db/migration/V4__supported_versions.sql
@@ -1,5 +1,2 @@
 ALTER TABLE server_properties
     ADD COLUMN supported_app_version character varying(255) NOT NULL;
-
-ALTER TABLE server_properties_aud
-    ADD COLUMN supported_app_version character varying(255);
