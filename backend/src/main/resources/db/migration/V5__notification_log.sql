24f9ae2a883d6f51900cf398c54ec6e8810c94f8
Radek Poskočil <r.poskocil@quadient.com>
2022-04-23 08:01:07 +0200

    Push notification logging

--- backend/src/main/resources/db/migration/V5__notification_log.sql
+++ backend/src/main/resources/db/migration/V5__notification_log.sql
@@ -8,10 +8,11 @@ CREATE SEQUENCE notification_log_seq
 CREATE TABLE notification_log (
                                   id bigint PRIMARY KEY DEFAULT nextval('notification_log_seq'),
                                   name text,
+                                  heading text,
                                   content text,
-                                  includeExternalUserIds text,
-                                  scheduleTimeOfDay text,
-                                  delayedOption text,
-                                  largeImage text,
-                                  iosAttachments text
+                                  include_external_user_ids text,
+                                  schedule_time_of_day text,
+                                  delayed_option text,
+                                  large_image text,
+                                  ios_attachments text
 );
